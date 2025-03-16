3215e3d37add85190222d1f861a1de5184ffbb4f
qin_qwq <150458910+qin-qwq@users.noreply.github.com>
2025-03-16 22:53:16 +0800

    Revenant fix and add

--- Roles/Neutral/Revenant.cs
+++ Roles/Neutral/Revenant.cs
@@ -15,11 +15,15 @@ internal class Revenant : RoleBase
     //==================================================================\\
 
     // private static OptionItem RevenantCanCopyAddons;
+    private static OptionItem CanKillKiller;
+
     public override void SetupCustomOption()
     {
         Options.SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Revenant);
         //RevenantCanCopyAddons = BooleanOptionItem.Create(Id + 10, "RevenantCanCopyAddons", false, TabGroup.NeutralRoles, false)
         //   .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Revenant]);
+        CanKillKiller = BooleanOptionItem.Create(Id + 11, "CanKillKiller", false, TabGroup.NeutralRoles, false)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.Revenant]);
     }
 
     public override bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target)
@@ -27,13 +31,22 @@ internal class Revenant : RoleBase
         CustomRoles role = killer.GetCustomRole();
         if (role.IsTNA()) return false;
 
-        killer.RpcMurderPlayer(killer);
-        killer.SetRealKiller(target);
+        if(CanKillKiller.GetBool())
+        {
+            killer.RpcMurderPlayer(killer);
+            killer.SetRealKiller(target);
+        }
 
+        killer.RpcGuardAndKill(target);
+
+        target.RpcGuardAndKill(target);
         target.GetRoleClass()?.OnRemove(target.PlayerId);
         target.RpcChangeRoleBasis(role);
         target.RpcSetCustomRole(role);
         target.GetRoleClass()?.OnAdd(target.PlayerId);
+
+        target.ResetKillCooldown();
+        target.SetKillCooldown(forceAnime: true);
 
         target.Notify(string.Format(GetString("RevenantTargeted"), Utils.GetRoleName(role)));
 
